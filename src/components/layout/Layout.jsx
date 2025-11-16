5823c5da0a252d151aea4a898b734c1dfb959a84
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-11-16 14:15:48 -0300

    fix(Sidebar): Fixando a sidebar para que não suma ao descer a pagina

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,7 +8,10 @@ const layoutStyle = {
 
 const contentStyle = {
   flex: 1,
-  padding: '20px'
+  padding: '20px',
+  overflowY: 'auto',  /* Adiciona a barra de rolagem AQUI */
+  height: '100vh',     /* Garante que a altura seja a da tela */
+  boxSizing: 'border-box' /* Impede problemas de padding */
 };
 
 export const Layout = ({ children }) => {
